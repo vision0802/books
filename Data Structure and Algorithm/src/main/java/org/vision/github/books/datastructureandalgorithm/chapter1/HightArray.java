8b660281e304ddf491eeacecce9299ceac4b25ba
vision0802 <617940236@qq.com>
2018-02-27 23:19:40 +0800

    remark: 添加注释。

--- Data Structure and Algorithm/src/main/java/org/vision/github/books/datastructureandalgorithm/chapter1/HightArray.java
+++ Data Structure and Algorithm/src/main/java/org/vision/github/books/datastructureandalgorithm/chapter1/HightArray.java
@@ -2,20 +2,22 @@ package org.vision.github.books.datastructureandalgorithm.chapter1;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /** @author ganminghui */
-
 public class HightArray {
-    /** 声明一个数组 */ private long[] a;
+    /** 声明一个数组 */
+    private long[] a;
 
-    /** 声明变量记录数据项个数 */ private int nElemts;
+    /** 声明变量记录数据项个数 */
+    private int nElemts;
 
     /** 构造方法用于初始化数组和数据项总数 */
     public HightArray(int max){ a = new long[max]; nElemts = 0; }
 
+    /** 新增元素到数组 */
     public void insert(long value){ a[nElemts] = value; nElemts ++ ; }
 
+    /** 查找元素 */
     public boolean find(long searchKey){
         int j;
 
@@ -24,6 +26,7 @@ public class HightArray {
         return j == nElemts;
     }
 
+    /** 删除元素,先查找元素,查找到,将后面的元素往前移动一步覆盖 */
     public boolean delete(long value){
         int j;
 
@@ -36,9 +39,9 @@ public class HightArray {
 
             return true;
         }
-
     }
 
+    /** 显示数组元素 */
     public void display(){
         System.out.println(Arrays.stream(a).mapToObj(String::valueOf).collect(Collectors.joining(" ")));
     }
--- Data Structure and Algorithm/src/main/java/org/vision/github/books/datastructureandalgorithm/chapter1/OrderArray.java
+++ Data Structure and Algorithm/src/main/java/org/vision/github/books/datastructureandalgorithm/chapter1/OrderArray.java
@@ -3,8 +3,7 @@ package org.vision.github.books.datastructureandalgorithm.chapter1;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-/** @author ganminghui */
-
+/** @author ganminghui、有序数组 */
 public class OrderArray {
     private long[] a;
 
@@ -14,6 +13,7 @@ public class OrderArray {
 
     public int size(){ return nElems; }
 
+    /** 有序数组新增先查找到待插入的位置,然后将最后一个位置到插入位置的数据集体往后移 */
     public void insert(long value){
         int j ;
         for (j = 0; j < nElems; j++) { if(a[j] > value) { break; } }
@@ -25,19 +25,25 @@ public class OrderArray {
         nElems++;
     }
 
+    /** 有序数组二分法查找 */
     public int find(long searchKey){
-        int lowerBound = 0, upperBound = nElems -1, curIndex;
+        int /** 低边界 */lowerBound = 0,
+            /** 高边界 */upperBound = nElems -1,
+            /** 当前索引 */curIndex;
 
         while (true){
             if(lowerBound > upperBound){ return nElems; }
 
             curIndex = (lowerBound + upperBound)/2;
             if(a[curIndex] == searchKey) { return curIndex; }
+            /** 如果待查找的数据大于中间值,将低边界值设置为中间值索引的后面一个索引 */
             else if (a[curIndex] < searchKey){ lowerBound = curIndex + 1; }
+            /** 如果待查找的数据小于中间值,将高边界值设置为中间值索引的前面一个索引 */
             else { upperBound = curIndex - 1; }
         }
     }
 
+    /** 有序数组删除先找到元素,找到后将找到元素的索引的后一个到最后的元素往前覆盖 */
     public boolean delete(long value){
         int j = find(value);
         if(j == nElems) { return false; }
@@ -47,6 +53,7 @@ public class OrderArray {
         return true;
     }
 
+    /** 有序数组显示元素 */
     public void display(){
         System.out.println(Arrays.stream(a).mapToObj(String::valueOf).collect(Collectors.joining(" ")));
     }
